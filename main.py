7fbabebe8b7b9d0854e492113f25e52b6fe02b09
dailybicycle <laruederia@gmail.com>
2019-12-16 20:04:38 -0500

    Draft. Centering of text resolved. Working on adjusting text to image size to avoid overun.

--- main.py
+++ main.py
@@ -11,42 +11,12 @@ assert os.path.exists(input_path), "I did not find the file at " + str(input_pat
 file = input_path
 print("File found.\n")
 
-placard = input("Type your placard or leave blank: ")
+placard = input("Type your placard text or leave blank: ")
 placard_standard = ["RESIST", "EMPATHY", "QUESTION", "VOTE", "REDISTRIBUTE WEALTH", "GIVE MORE", "WASTE LESS",
                     "MIGHT ≠ RIGHT"]
-
 if placard == "":
     placard = random.choice(placard_standard)
     # https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
-
-# def wrap(text, font, max_width):
-#     phrase = []
-#     # If the width of the text is smaller than image width
-#     # no need to split it, just add it to the lines array and return.
-#     if font.getsize(text)[0] <= max_width:
-#         phrase.append(text)
-#     else:
-#         # split the line by spaces to get separate words
-#         words = text.split(' ')
-#         i = 0
-#         # append every word to a line while the total width is less than image width
-#         while i < len(words):
-#             line = ''  # if there's no text, line is blank
-#             while i < len(words) and font.getsize(line + words[i])[0] <= max_width:
-#                 line = line + words[i] + " "  # adds new words to each line and a space at the end
-#                 i += 1
-#             if not line:
-#                 line = words[i]
-#                 i += 1
-#             # when the line gets longer than the max width stop appending the word,
-#             # and add it to a new line in the lines array
-#             phrase.append(line)
-#     return phrase
-#     # draw the message on the background
-
-
-# initialize the drawing context with
-# the image object as background
 
 img = Image.open(input_path)
 draw = ImageDraw.Draw(img)
@@ -57,23 +27,40 @@ W, H = img.size  # tuple of image dimensions; essential that width is before hei
 size_font = int(H / 4)  # maintains the proportion of text relative to image size
 color = 'rgb(255, 255, 255)'  # white color
 placard_font = ImageFont.truetype('HelveticaNeue.ttc', size=size_font, index=1)  # index here indicates
+line_height = placard_font.getsize('pd')[1]  # gives total line height; p & d reach extents
 # the style within the TrueType collection file sequence.
-placard_spacing = (H - (size_font * 3)) / 2
+placard_spacing = (H - (size_font * 3)) / 3
 # fit lines to image
 
-para = textwrap.wrap(placard, width=8, max_lines=3)
-print(para)
+text = []
 
-i = 0
-for line in para:
-    while i < len(para):
-        w, h = draw.textsize(para()[i], font=placard_font, spacing=placard_spacing)
+if placard_font.getsize(placard)[0] < W:
+    text.append(placard)
+else:
+    # split the line by spaces to get separate words
+    words = placard.split(' ')
+    i = 0
+    # append every word to a line while the total width is less than image width
+    while i < len(words):
+        line = ''  # if there's no text, line is blank
+        while i < len(words) and placard_font.getsize(line + words[i])[0] <= W:
+            line = line + words[i] + " "  # adds new words to each line and a space at the end
+            i += 1
+        if not line:
+            line = words[i]
+            i += 1
+        # when the line gets longer than the max width stop appending the word,
+        # and add it to a new line in the lines array
+        text.append(line)
+print(text)
+y = (H - (line_height * len(text) + placard_spacing * (len(text)-1)))/2
 
-        # starting position of the message
-        (x, y) = ((W - w) / 2, (H - h) / 2)  # centers the text on the image
-
-        draw.multiline_text(xy=(x, y), text=para()[i], fill=color, font=placard_font, align="center")
-        i += 1
+for line in text:
+    line_width = draw.textsize(line, font=placard_font)[0]
+    # starting position of the message
+    x = (W - line_width) / 2  # centers the text on the image
+    draw.multiline_text(xy=(x, y), text=line, fill=color, font=placard_font, align="center")
+    y = y + line_height  # + placard_spacing
 
     # save the edited image
 if input_path.find('.jpg'):
